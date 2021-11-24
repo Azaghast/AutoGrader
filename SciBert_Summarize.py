528f59cc8d1865f0881e1f965f164a60323c4822
Azaghast <kunae47@gmail.com>
2021-11-24 19:22:22 +0000

    Add files via upload

---
+++ SciBert_Summarize.py
@@ -0,0 +1,19 @@
+from summarizer import Summarizer
+from transformers import *
+
+class SciBert_Summarizer():
+
+    scibert_config = AutoConfig.from_pretrained('allenai/scibert_scivocab_uncased')
+    scibert_config.output_hidden_states=True
+    scibert_tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+    scibert = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased',config=scibert_config)
+    model = Summarizer(custom_model=scibert,custom_tokenizer=scibert_tokenizer)
+    
+    def summarize(context,num_sentences):
+        summary = model(context.lower(),num_sentences)
+        return summary
+    
+    if __name__ == '__main__':
+        text = 'hi hello'
+        summ = summarize(text,1)
+        print(summ)
